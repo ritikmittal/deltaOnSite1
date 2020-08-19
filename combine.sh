31808c52560fee2649a10403bff1f5b55062cc15
Ritik Mittal <ritikmittal@github.com>
2020-08-19 14:21:03 +0530

    Upadated

--- combine.sh
+++ combine.sh
@@ -5,23 +5,25 @@ file1=$1
 file2=$2
 file3=$3
 # file1 has name, email, rollno
-# file2 has rollno, phoneno
+# file2 has rollno, branch
 # file3 har email, phoneno, year
 
 function addToFile {
    #read from file 3
    while read email phoneno year; do
-      if [ $email == $4 ] && [ $phoneno == $3 ]; then
-         echo "$1 $2 $3 $4 $year" >> /Users/ritikmittal/Desktop/bin/deltaonline1/output.txt
+      if [ $email == $4 ] ; then
+         echo "$1 $2 $3 $4 $phoneno $year" >> output.txt
+         break;
       fi
    done < $file3
 }
 
 function write_phone {
-   while read rollno phoneno; do
+   while read rollno branch; do
       #statements
       if [ $rollno == $3 ] ; then
-         addToFile $rollno $1 $phoneno $2
+         addToFile $rollno $1 $branch $2
+         break;
       fi
    done < $file2
 }
--- search.sh
+++ search.sh
@@ -4,14 +4,16 @@ file=$1
 
 context=$2
 expression=$3
-while read rollno name phoneno email year; do
+while read rollno name branch email phoneno year; do
    if [[ $context == 'roll:' ]] && [[ $rollno == $expression* ]] ; then
       echo "$rollno $name   $phoneno $email   $year"
-   elif [[ $context == 'phno:' ]] && [[ $phoneno == $expression* ]] ; then
+   elif [[ $context == 'phoneno:' ]] && [[ $phoneno == $expression* ]] ; then
       echo "$rollno $name   $phoneno $email   $year"
-   elif [[ $context == 'mail:' ]] && [[ $email == $expression* ]] ; then
+   elif [[ $context == 'email:' ]] && [[ $email == $expression* ]] ; then
       echo "$rollno $name   $phoneno $email   $year"
    elif [[ $context == 'year:' ]] && [[ $year == $expression* ]] ; then
+      echo "$rollno $name   $phoneno $email   $year"
+   elif [[ $context == 'branch:' ]] && [[ $branch == $expression* ]] ; then
       echo "$rollno $name   $phoneno $email   $year"
    fi
 done < $file
