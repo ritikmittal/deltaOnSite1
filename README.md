27ae5167eb37d2b5efafdd2d30b17f62c93ecefe
Ritik Mittal <ritikmittal@github.com>
2020-08-19 18:08:16 +0530

    Updated version

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ combine.sh is used to combine file1,2 and 3 and takes 3 input
 sample command:
 ./combine.sh file1.txt file2.txt file3.txt
 
-search takes 3 input 
+search takes 3 input
 sample command:
 
-./search.sh output.txt roll: 106
+./search.sh output.txt roll:106
+./search.sh output.txt phoneno:9
+./search.sh output.txt email:mit
+./search.sh output.txt year:202
+./search.sh output.txt branch:cse
+./search.sh output.txt name:Nit
--- search.sh
+++ search.sh
@@ -2,8 +2,27 @@
 
 file=$1
 
-context=$2
-expression=$3
+query=$2
+if [[ $query =~ 'roll' ]] ; then
+   context='roll:'
+   expression=${query:5}
+elif [[ $query =~ 'phon' ]] ; then
+   context='phoneno:'
+   expression=${query:8}
+elif [[ $query =~ 'email' ]] ; then
+   context='email:'
+   expression=${query:6}
+elif [[ $query =~ 'year' ]] ; then
+   context='year:'
+   expression=${query:5}
+elif [[ $query =~ 'branch' ]] ; then
+   context='branch:'
+   expression=${query:6}
+elif [[ $query =~ 'name' ]]; then
+   context='name:'
+   expression=${query:5}
+fi
+
 while read rollno name branch email phoneno year; do
    if [[ $context == 'roll:' ]] && [[ $rollno == $expression* ]] ; then
       echo "$rollno $name   $phoneno $email   $year"
@@ -14,6 +33,8 @@ while read rollno name branch email phoneno year; do
    elif [[ $context == 'year:' ]] && [[ $year == $expression* ]] ; then
       echo "$rollno $name   $phoneno $email   $year"
    elif [[ $context == 'branch:' ]] && [[ $branch == $expression* ]] ; then
+      echo "$rollno $name   $phoneno $email   $year"
+   elif [[ $context == 'name:' ]] && [[ $name == $expression* ]]; then
       echo "$rollno $name   $phoneno $email   $year"
    fi
 done < $file
